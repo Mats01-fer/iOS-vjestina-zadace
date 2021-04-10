03a00773a03c2040078bcf2f549763880df698a2
Mats01-fer <matej.butkovic@fer.hr>
2021-04-10 18:32:05 +0200

    adds LoginViewCotroller

---
+++ QuizApp/LoginView/LoginViewController.swift
@@ -0,0 +1,32 @@
+//
+//  LoginViewController.swift
+//  QuizApp
+//
+//  Created by Matej Butkovic on 10.04.2021..
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func loginAction(_ sender: Any) {
+        print("les go")
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
